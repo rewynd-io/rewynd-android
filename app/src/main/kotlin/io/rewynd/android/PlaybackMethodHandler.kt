ea047d32c2aa6b3118d7b778af35ba649035f951
kensand <git@kensand.net>
2023-06-18 22:12:10 -0400

    Revert "Start completed shows at the beginning"
    This reverts commit 8f18c05d2823c372c628af742d6948541e7a431a.

--- app/src/main/kotlin/io/rewynd/android/PlaybackMethodHandler.kt
+++ app/src/main/kotlin/io/rewynd/android/PlaybackMethodHandler.kt
@@ -4,30 +4,26 @@ import kotlin.time.Duration.Companion.seconds
 
 // TODO figure out the nice inheritance/sealed interface way of doing this
 object PlaybackMethodHandler {
-    suspend fun next(client: RewyndClient, playerMedia: PlayerMedia): PlayerMedia? =
-        when (playerMedia) {
-            is PlayerMedia.Episode -> EpisodePlaybackMethodHandler.next(client, playerMedia)
-        }
-
-    suspend fun prev(client: RewyndClient, playerMedia: PlayerMedia): PlayerMedia? =
-        when (playerMedia) {
-            is PlayerMedia.Episode -> EpisodePlaybackMethodHandler.prev(client, playerMedia)
-        }
+    suspend fun next(client: RewyndClient, playerMedia: PlayerMedia): PlayerMedia? = when(playerMedia) {
+        is PlayerMedia.Episode -> EpisodePlaybackMethodHandler.next(client, playerMedia)
+    }
+    suspend fun prev(client: RewyndClient, playerMedia: PlayerMedia): PlayerMedia? = when(playerMedia) {
+        is PlayerMedia.Episode -> EpisodePlaybackMethodHandler.prev(client, playerMedia)
+    }
 }
 
-private object EpisodePlaybackMethodHandler : KLog() {
+private object EpisodePlaybackMethodHandler : KLog()  {
     suspend fun next(
         client: RewyndClient,
         playerMedia: PlayerMedia.Episode
     ): PlayerMedia.Episode? = try {
         client.getNextEpisode(playerMedia.info.id).body().let {
             val progress = client.getUserProgress(it.id).body()
-                .run { (if (percent > 0.9) 0.0 else percent) }
             PlayerMedia.Episode(
                 playbackMethod = playerMedia.playbackMethod,
                 info = it,
                 runTime = it.runTime.seconds,
-                startOffset = (progress * it.runTime).seconds
+                startOffset = (progress.percent * it.runTime).seconds
             )
         }
     } catch (e: Exception) {
